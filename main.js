096d458dc0473ab2f305c9ac795c90d6b6517755
Misbah Shakil <141862743+misbahshakil233@users.noreply.github.com>
2024-04-06 00:02:31 +0500

    Add files via upload
    todoApp

---
+++ main.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            message: "What do you want to add to your todo list?",
+            type: "input"
+        },
+        {
+            name: "addMore",
+            message: "Are you sure you want to add another task?",
+            type: "confirm",
+            default: false
+        }
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    // Display todos as a list
+    console.log("\nYour Todo List:");
+    todos.forEach((task, index) => {
+        console.log(`${index + 1}. ${task}`);
+        condition = true;
+    });
+}
+;
---
+++ ownSelf.js
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+//ask todo
+//tell todo
+//ptnt in show just like list
+//ask todo
+let todo = [];
+let condition = true;
+while (condition) {
+    let addTodo = await inquirer.prompt([
+        {
+            name: "firstQuestuin",
+            message: "what do you want to add in todos:",
+            type: "input"
+        },
+        {
+            name: "secondQuestion",
+            message: "would you add or more ? ",
+            type: "confirm",
+            default: "true"
+        }
+    ]);
+    //tell todo
+    todo.push(addTodo.firstQuestuin);
+    //put in show just like list
+    todo.forEach((task, index) => {
+        console.log(chalk(`${index + 1}. ${task}`));
+        condition = addTodo.secondQuestion;
+    });
+}
